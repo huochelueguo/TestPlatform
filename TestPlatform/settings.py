941aed6f8953561ae72a6c9e3b99b00dde00df08
Administrator <651809271@qq.com>
2021-09-09 19:04:42 +0800

    之前的跨域配置 vue+django

--- TestPlatform/settings.py
+++ TestPlatform/settings.py
@@ -38,17 +38,65 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'background.apps.BackgroundConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+
+    'google.com',
+
+    'hostname.example.com'
+
+)
+
+CORS_ORIGIN_WHITELIST = ()
+
+CORS_ORIGIN_REGEX_WHITELIST = ()
+
+CORS_ALLOW_METHODS = (
+
+    'GET',
+
+    'POST',
+
+    'PUT',
+
+    'PATCH',
+
+    'DELETE',
+
+    'OPTIONS'
+
+)
+
+CORS_ALLOW_HEADERS = (
+
+    'x-requested-with',
+
+    'content-type',
+
+    'accept',
+
+    'origin',
+
+    'authorization',
+
+    'x-csrftoken'
+
+)
 
 ROOT_URLCONF = 'TestPlatform.urls'
 
